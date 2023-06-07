3f74036c4a95055e8808e92d6c5eca37826be554
Anton <ponochovnyi.anton@LLL.kpi.ua>
2023-06-07 15:51:20 +0300

    Update

---
+++ front.js
@@ -0,0 +1,32 @@
+
+//Catalog
+
+//SortingButton
+const dropdown = document.querySelectorAll('.Sorting-dropdown');
+dropdown.forEach(dropdown => {
+
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelector('.menu li');
+    const selected = dropdown.querySelector('.selected');
+
+
+    select.addEventListener('click' , () => {
+
+        select.classList.toggle('caret-clicked');
+
+        caret.classList.toggle('caret-rotate');
+
+        menu.classList.toggle('menu-open');
+    });
+
+    options.forEach(option => {
+        option.addEventListener('click',() => {
+            selected.innerText = option.innerText;
+            select.classList.remove('select-clicked');
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('menu-open');
+        })
+    })
+})
