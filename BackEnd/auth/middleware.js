97d7a908d2dd4d52597c17bfe0c2dfd79274b2a2
Taras <turchenko730@gmail.com>
2024-04-29 16:17:53 +0300

    changed logic if not authenticated

--- BackEnd/auth/middleware.js
+++ BackEnd/auth/middleware.js
@@ -2,9 +2,7 @@ module.exports.isAuth = (req, res, next) => {
   if (req.isAuthenticated()) {
     next()
   } else {
-    res
-      .status(401)
-      .json({ msg: "You are not authorized to view this resource" })
+    res.redirect("/login.html")
   }
 }
 
