eaa42df7b125734705dbd8823c50ce597a257bbc
Taras <turchenko730@gmail.com>
2024-05-26 00:59:43 +0300

    fixed setup test

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -1,20 +1,11 @@
 // test/setupTests.js
 const request = require("supertest")
-const { app, closeServer } = require("../BackEnd/server")
+const db = require("../BackEnd/database")
+const { app } = require("../BackEnd/server")
 require("dotenv").config()
 
-const PORT = process.env.TEST_PORT || 4000 // Default to 4000 if TEST_PORT is not set
-let server
-
-beforeAll((done) => {
-  server = app.listen(PORT, () => {
-    console.log(`Test server running on http://localhost:${PORT}`)
-    done()
-  })
-})
-
 afterAll(async () => {
-  await closeServer(server)
+  await db.promisePool.end()
 })
 
 module.exports = request(app)
