f0673da9eee6fec3e59672afd9c0c9398d816c26
Taras <turchenko730@gmail.com>
2024-05-26 20:22:06 +0300

    fixed realisation of db for tests

--- tests/product.test.js
+++ tests/product.test.js
@@ -4,6 +4,7 @@ const request = require("./setupTests")
 describe("GET /api/v1/products", () => {
   it("should return all products", async () => {
     const res = await request.get("/api/v1/products")
+    console.log(res.body.result)
     expect(res.statusCode).toBe(200)
     expect(res.body.result.length).toBeGreaterThan(0)
   })
